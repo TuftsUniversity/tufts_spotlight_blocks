9dbdf0ded6e448a413f002a99c9f110d6b9e4709
Travis Lilleberg <travis.lilleberg@tufts.edu>
2017-02-09 10:45:57 -0500

    Removing stuff we're never going to use.

--- app/jobs/tufts_spotlight_blocks/application_job.rb
+++
@@ -1,4 +0,0 @@
-module TuftsSpotlightBlocks
-  class ApplicationJob < ActiveJob::Base
-  end
-end
--- test/tufts_spotlight_blocks_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TuftsSpotlightBlocks::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, TuftsSpotlightBlocks
-  end
-end
